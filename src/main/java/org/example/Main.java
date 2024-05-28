485632393670671c2c780458dce1ac1236c707ce
MarcoSkiadas <Skiadas2@hotmail.com>
2024-05-28 10:34:22 +0200

    adding checkPasswordDigits + Tests

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
 package org.example;
 
+/** checkPasswordDigits
+ * checkPasswordCases
+ * checkCommonPassword
+ */
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(checkPasswordDigits("Test1234"));
+    }
+    public static boolean checkPasswordLength(String password) {
+        if (password.length() < 8) {
+            return false;
+        }
+        return true;
+    }
+    public static boolean checkPasswordDigits(String password) {
+       for (int i = 0; i < password.length(); i++) {
+           if (Character.isDigit(password.charAt(i))) {
+               return true;
+           }
+       }
+       return false;
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -9,9 +9,9 @@ class MainTest {
     @Test
     void checkPasswordLength_shouldReturnTrue_whenCalledWith8Chars(){
       //GIVEN
-        boolean expected = true
+        boolean expected = true;
       //WHEN
-        boolean actual = checkPasswordLength.lengthTest(8)
+        boolean actual = Main.checkPasswordLength("Test1234");
       //THEN
       assertEquals(expected,actual);
     }
@@ -19,20 +19,33 @@ class MainTest {
     @Test
     void checkPasswordLength_shouldReturnFalse_whenCalledWith7Chars(){
         //GIVEN
-        boolean expected = false
+        boolean expected = false;
         //WHEN
-        boolean actual = checkPasswordLength.lengthTest(7)
+        boolean actual = Main.checkPasswordLength("Test123");
         //THEN
         assertEquals(expected,actual);
     }
     @Test
     void checkPasswordLength_shouldReturnTrue_whenCalledWith10Chars(){
         //GIVEN
-        boolean expected = true
+        boolean expected = true;
         //WHEN
-        boolean actual = checkPasswordLength.lengthTest(10)
+        boolean actual = Main.checkPasswordLength("Test123456");
         //THEN
         assertEquals(expected,actual);
     }
-
+    @Test
+    void checkPasswordDigits_shouldReturnTrue_whenCalledWithDigits(){
+        boolean expected = true;
+        boolean actual = Main.checkPasswordDigits("Test1234");
+        assertEquals(expected,actual);
+    }
+    @Test
+    void checkPasswordDigits_shouldReturnFalse_whenCalledWithoutDigits(){
+        boolean expected = false;
+        boolean actual = Main.checkPasswordDigits("TestTest");
+        assertEquals(expected,actual);
+    }
+    @Test
+    void checkPasswordCases_shouldReturnTrue_whenCalledWithUppercaseDigits(){}
 }
