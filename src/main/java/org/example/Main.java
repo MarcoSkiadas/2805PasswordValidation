5740c176194fa756f8ecd427773fc9b0a6df51b4
MarcoSkiadas <Skiadas2@hotmail.com>
2024-05-28 12:18:06 +0200

    Adding Comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,23 +2,20 @@ package org.example;
 
 import java.util.Scanner;
 
-/** checkPasswordDigits
- * checkPasswordCases
- * checkCommonPassword
- */
-
-
 public class Main {
     public static void main(String[] args) {
+        //Output with requested input from user to choose a Password
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your password: ");
         String password = sc.nextLine();
         System.out.println(passwordFeedback(password));
 
     }
+    //Check if the password length is True
     public static boolean checkPasswordLength(String password) {
         return password.length() >= 8;
     }
+    //Check if the password contains digits
     public static boolean checkPasswordDigits(String password) {
        for (int i = 0; i < password.length(); i++) {
            if (Character.isDigit(password.charAt(i))) {
@@ -27,6 +24,7 @@ public class Main {
        }
        return false;
     }
+    //Check the password if uppercases are contained
     public static boolean checkPasswordUpperCases(String password) {
         for (int i = 0; i < password.length(); i++) {
             if (Character.isUpperCase(password.charAt(i))) {
@@ -35,6 +33,7 @@ public class Main {
         }
         return false;
     }
+    //Check the password if lowercases are contained
     public static boolean checkPasswordLowerCases(String password) {
         for (int i = 0; i < password.length(); i++) {
             if (Character.isLowerCase(password.charAt(i))) {
@@ -43,6 +42,7 @@ public class Main {
         }
         return false;
     }
+    //check if this password contains commons words or is insecure
     public static boolean checkCommonPassword(String password) {
         String[] common = {"password","swordfish","test","hello"};
         String passwordLow = password.toLowerCase();
@@ -65,6 +65,7 @@ public class Main {
         }
         return true;
     }
+    //check if the password contains special Characters
     public static boolean checkSpecialCharacters(String password) {
         for (int i = 0; i < password.length(); i++) {
             if (!Character.isDigit(password.charAt(i)) && !Character.isLetter(password.charAt(i)) && !Character.isWhitespace(password.charAt(i))) {
@@ -73,6 +74,7 @@ public class Main {
         }
         return false;
     }
+    // Output Method to give the User feedback
     public static String passwordFeedback(String password) {
         if (!checkPasswordLength(password)) {
             return "Your Password: "+ password + " must contain at least 8 characters";
